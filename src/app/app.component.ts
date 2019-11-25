37d7cb52a06f6e68f983de8b7f001715118df92b
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-11-24 21:52:16 -0500

    Reactive form assignment 7 - solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng8-forms-reactive-assignment07';
+export class AppComponent implements OnInit {
+  statusOptions = [
+    {value: 'stable', label: 'Stable'},
+    {value: 'critical', label: 'Critical'},
+    {value: 'finished', label: 'Finished'}
+  ];
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(
+          null, 
+          [Validators.required, CustomValidators.invalidProjectName],
+          CustomValidators.asyncInvalidProjectName),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('critical')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
